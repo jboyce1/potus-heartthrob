58fac9c1d4a812a54f1b92e28e99981fb899b0b8
jboyce1 <79017276+jboyce1@users.noreply.github.com>
2026-05-23 09:27:37 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("POTUS Heartthrob script loaded.");
+
+function testButton() {
+  const output = document.getElementById("js-test-output");
+  output.textContent = "JavaScript is working. The republic is emotionally available.";
+}
